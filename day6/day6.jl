5c1f842c47163a711ae999691b1a506ec22e82a2
Semyeong Oh <semyeong.oh@gmail.com>
2020-12-06 16:42:06 +0000

    do block

--- day6/day6.jl
+++ day6/day6.jl
@@ -1,10 +1,11 @@
 
 
-f = open("input.txt")
-s = read(f, String)
-groups = split.(split(s, "\n\n"))
-count = map(g -> length(Set(collect(join(g)))), groups)
-println(sum(count))    # 6351
+open("input.txt") do f
+    s = read(f, String)
+    groups = split.(split(s, "\n\n"))
+    count = map(g -> length(Set(collect(join(g)))), groups)
+    println(sum(count))    # 6351
 
-count2 = map(g -> length( reduce(intersect, collect.(g)) ), groups)
-println(sum(count2)) # 3143
+    count2 = map(g -> length( reduce(intersect, collect.(g)) ), groups)
+    println(sum(count2)) # 3143
+end
