3f47374252e523876e6be44da652032e0567d6d5
Semyeong Oh <semyeong.oh@gmail.com>
2020-12-15 20:35:42 +0000

    remove global scope; 18s -> 2.2s

--- day15/part2.jl
+++ day15/part2.jl
@@ -1,34 +1,26 @@
 # --- part 2
 # let's only keep the last time each number was spoken
 
-gg = Dict(
-    5 => 1,
-    1 => 2,
-    9 => 3,
-    18 => 4,
-    13 => 5,
-    8 => 6,
-    "last" => 0,
-    "lastturn" => 7
-)
+function nextturn2!(g, last, i)
+    n = i - get(g, last, i)
+    g[last] = i
+    g, n, i+1
+end
 
-
-function nextturn2!(g)
-    if !(g["last"] in keys(g))
-        g[g["last"]] = g["lastturn"]
-        g["last"] = 0
-        g["lastturn"] = g["lastturn"] + 1
-    else
-        i = g["lastturn"] - g[g["last"]]
-        g[g["last"]] = g["lastturn"]
-        g["last"] = i
-        g["lastturn"] = g["lastturn"] + 1
+let n = 0, i = 7
+    gg = Dict(
+        5 => 1,
+        1 => 2,
+        9 => 3,
+        18 => 4,
+        13 => 5,
+        8 => 6,
+        0 => 7
+    )
+    while (i != 30000000)
+        gg, n, i = nextturn2!(gg, n, i)
     end
+    println(n)
 end
-
-
-while (gg["lastturn"] != 30000000)
-    nextturn2!(gg)
-end
-println(gg["last"])   # 323780
+# println(gg["last"])   # 323780
 
